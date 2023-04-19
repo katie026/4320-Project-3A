0b92ecd5febed3ca090ce185e5def9b521cc83a0
katie026 <>
2023-04-19 01:16:26 -0500

    got flash messages to work

--- app.py
+++ app.py
@@ -26,8 +26,10 @@ def index():
         time_series = request.form["time_series"]
 
         date_format = "%Y-%m-%d"
-        start_date = datetime.strptime(request.form.get("start_date", ""), date_format)
-        end_date = datetime.strptime(request.form.get("end_date", ""), date_format)
+        start_date_str = request.form.get("start_date")
+        start_date = datetime.strptime(start_date_str, date_format) if start_date_str else None
+        end_date_str = request.form.get("end_date")
+        end_date = datetime.strptime(end_date_str, date_format) if end_date_str else None
         
         # validate form data and flash error message if error
         if symbol == "":
@@ -36,17 +38,19 @@ def index():
             flash("Chart Type is required.")
         elif(time_series == ""):
             flash("Time Series is required.")
-        elif(start_date == ""):
+        elif(start_date is None):
             flash("Start Date is required.")
-        elif(end_date == ""):
+        elif(end_date is None):
             flash("End Date is required.")
-        elif(start_date < end_date):
-            flash("Start Date cannot be later than End Date.")
+        elif(end_date <= start_date):
+            flash("End Date must be after Start Date.")
         else:
             # if no errors, query the API
             stocksDictionary = stockVisualizer.getData(time_series, symbol)
             stockVisualizer.generateGraph(symbol, time_series, chart_type, stocksDictionary, start_date, end_date)
-
+        
+        # get SP500 symbol list
+        sp500_symbols = symbols.getSP500SymbolsFromWiki()
         # render index.html and pass SP500 symbol list to index.html template as a variable
         return render_template('index.html', sp500_symbols=sp500_symbols)
         
